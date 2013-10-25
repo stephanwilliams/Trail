5264b4b7f72ae5e9e141454d4bfc25bd2ac0f75f
Michael Lu <lumichael94@gmail.com>
2013-10-25 19:53:29 -0400

    Added /rooms/ to routes

--- routes.js
+++ routes.js
@@ -1,3 +1,6 @@
 exports = module.exports = function(app){
 	app.get('/',require('./views/index').init);
 }
+exports = module.exports = function(app){
+	app.get('/rooms/', require('./views/rooms/index').init);
+}
