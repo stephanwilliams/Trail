73d5aeec37650bdbd7956ac3b020e434234a5fa2
Stephan Williams <stephanwilliams.93@gmail.com>
2013-10-25 23:05:28 -0400

    createNewRoom and createNewPost
    Added functions for creating new rooms and new posts

--- public/js/trail.js
+++ public/js/trail.js
@@ -1,9 +1,10 @@
 var trail = new Firebase('https://trail.firebaseio.com/');
-var rooms = trail.child('room_ids');
+var roomIds = trail.child('room_ids');
+var rooms = trail.child('rooms');
 var currentRoom;
 var currentRoomValueCache;
 
-rooms.on('value', refreshRoomsList);
+roomIds.on('value', refreshRoomsList);
 
 function refreshRoomsList(snapshot) {
     $.each(snapshot.val(), function(roomName, roomId) {
@@ -18,7 +19,90 @@ $('#ROOM_BUTTON_ID').on('click', function(event) {
     currentRoom.on('value', refreshCurrentRoom);
 });
 
+// takes the room id as a string
+function changeCurrentRoom(newRoomId) {
+    if (currentRoom != null) currentRoom.off('value', refreshCurrentRoom);
+    currentRoom = trail.child('rooms').child(newRoomId);
+    currentRoom.on('value', refreshCurrentRoom);
+}
+
 function refreshCurrentRoom(snapshot) {
     currentRoomValueCache = snapshot.val();
     // refresh the current room
+}
+
+/**
+ * Adds a post to Firebase.
+ *
+ * Returns (via callback) a reference to the new post if the
+ * transaction was successful, or an error string if posting
+ * failed.
+ */
+function createNewRoom(roomName, userId, content, callback) {
+    var newRoom = rooms.push();
+    roomIds.transaction(function(currentRooms) {
+        if (!$.isPlainObject(currentRooms)) {
+            currentRooms = {};
+        }
+        if (roomName in currentRooms) {
+            callback('A room with that name already exists!');
+            return;
+        }
+        currentRooms[roomName] = newRoom.name();
+        return currentRooms;
+    }, function (error, committed, snapshot) {
+        if (error !== null || !committed) {
+            if (typeof callback === 'function')
+                callback('Room creation failed!' +
+                    '<br />Was there an error? ' + error +
+                    '<br />Were the changes committed? ' + committed);
+            return;
+        } else {
+            changeCurrentRoom(newRoom.name());
+            createNewPost(newRoom.name(), userId, content, null,
+                function(value) {
+                    if (typeof value === 'string') { // there was an error
+                        if (typeof callback === 'function')
+                            callback('Failed creating initial post!<br />' + value);
+                        return;
+                    }
+                });
+        }
+    });
+    if (typeof callback === 'function') callback(newRoom);
+}
+
+/**
+ * Adds a post to Firebase.
+ *
+ * Returns (via callback) a reference to the new post if the
+ * transaction was successful, or an error string if posting
+ * failed.
+ */
+function createNewPost(roomId, userId, content, parents, callback) {
+    var newPost = currentRoom.child('posts').push({
+        'user_id': userId,
+        'ts': +new Date,
+        'content': content,
+        'parents': parents
+    });
+    $.each(parents, function(index, parentId) {
+        var children = currentRoom.child('posts').child(parentId).child('children');
+        children.transaction(function(currentChildren) {
+            if (!$.isArray(currentChildren)) {
+                currentChildren = [];
+            }
+            currentChildren.push(newPost.name());
+            return currentChildren;
+        }, function(error, committed, snapshot) {
+            if (error !== null || !committed ) {
+                if (typeof callback === 'function')
+                    callback('Post creation failed!' +
+                        '<br />Was there an error? ' + error +
+                        '<br />Were the changes committed? ' + committed);
+                return;
+            }
+        });
+    });
+    if (typeof callback === 'function') callback(newPost);
 }
