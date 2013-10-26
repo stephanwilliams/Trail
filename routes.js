5c1bce527ce08261ba05037d1ad4154f4b67aa58
Paul Park <camo1018@yahoo.com>
2013-10-25 20:04:01 -0400

    Added rendering to rooms and updated routes

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 exports = module.exports = function(app){
 	app.get('/',require('./views/index').init);
-}
-exports = module.exports = function(app){
+
 	app.get('/rooms/', require('./views/rooms/index').init);
 }
---
+++ views/rooms/index.js
@@ -0,0 +1,3 @@
+exports.init = function(req,res){
+	res.render('rooms/index.html');
+}
