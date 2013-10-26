d8e7c6570b81ae8915fea47fa73b6d5680bb87e1
Michael Lu <lumichael94@gmail.com>
2013-10-25 23:12:02 -0400

    New route /join/

--- routes.js
+++ routes.js
@@ -2,4 +2,6 @@ exports = module.exports = function(app){
 	app.get('/',require('./views/index').init);
 
 	app.get('/rooms/', require('./views/rooms/index').init);
+
+	app.get('/join/', require('./views/join/index').init);
 }
